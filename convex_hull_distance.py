21401b44559fe04b34f860e5bc272711df0a7fef
Tytz <44804681+calibertytz@users.noreply.github.com>
2020-09-05 10:04:42 +0800

    Add files via upload
    first commit.

---
+++ convex_hull_distance.py
@@ -0,0 +1,35 @@
+import numpy as np
+import scipy
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
+
+
+def compute(target, convex_hull):
+    l = convex_hull.shape[0]
+
+    def obj(x):
+        result = np.dot(np.diag(x), target - convex_hull)
+        return np.linalg.norm(result)
+
+    # 不等式约束
+    ineq_cons = {"type": "ineq",
+                 "fun": lambda x: -x}
+
+    # 等式约束
+    eq_cons = {"type": "eq",
+               "fun": lambda x: sum(x)-1}
+
+    x0 = np.ones(l)/l
+
+    res = minimize(obj, x0, method='SLSQP', constraints=[eq_cons, ineq_cons], options={'ftol': 1e-9, 'disp': False})
+
+    return res.fun
+
+
+
+
+if __name__ == '__main__':
+    X, Y = np.random.random(size=(3, 4)), np.random.normal(size=4)
+    print(compute(target=Y, convex_hull=X))
+
+
